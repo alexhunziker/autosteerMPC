172449fd169025161f8411f5b609ae26dfe54812
alexhunziker91@gmail.com <alexhunziker91@gmail.com>
2019-06-16 23:44:55 +0200

    Added image warper #cv

---
+++ cv/src/image_warper.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+
+class ImageWarper(object):
+    """
+    Warps binary image into bird-view
+    """
+    DEFAULT_DESTINATION_SIZE = (1280, 720)
+    DEFAULT_SOURCE_ROI = np.float32([(0.43, 0.65), (0.58, 0.65), (0.1, 1), (1, 1)])
+    DEFAULT_DESTINATION_ROI = np.float32([(0, 0), (1, 0), (0, 1), (1, 1)])
+
+    @classmethod
+    def warp(cls, img,
+                              destination_size=DEFAULT_DESTINATION_SIZE,
+                              source_roi_proportion=DEFAULT_SOURCE_ROI,
+                              destination_roi_proportion=DEFAULT_DESTINATION_ROI
+                              ):
+        """
+        Warps an image according to the specified ROIs (if any)
+        :param img: Binary image to be processed
+        :param destination_size: size of output image
+        :param source_roi_proportion
+        :param destination_roi_proportion
+        :return: warped image
+        """
+        source_size = np.float32([(img.shape[1], img.shape[0])])
+        source_roi = source_roi_proportion * source_size
+        destination_roi = destination_roi_proportion * np.float32(destination_size)
+        transformation_matrix = cv2.getPerspectiveTransform(source_roi, destination_roi)
+        return cv2.warpPerspective(img, transformation_matrix, destination_size)
