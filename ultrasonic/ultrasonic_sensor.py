2365c7c2ecfec0bb8eb5f30fb3c5f136cd8cd6a2
alexhunziker91@gmail.com <alexhunziker@sunrise.ch>
2019-07-25 10:37:34 +0200

    ultra sonic sensor with threading #sensor

--- ultrasonic/ultrasonic_sensor.py
+++ ultrasonic/ultrasonic_sensor.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
 import time
+import threading
+
 
 class UltrasonicSensor(object):
-    GPIO_PIN = 6
+    GPIO_PIN = 2
     TIMEOUT_1 = 1000
     TIMEOUT_2 = 10000
 
@@ -10,6 +12,16 @@ class UltrasonicSensor(object):
 
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
+        self.stop = False
+        measure_thread = threading.Thread(target=self.measure_loop)
+        measure_thread.start()
+
+    def measure_loop(self):
+        time.sleep(1)
+        while not self.stop:
+            self.measured_distance = self.measure()
+            UltrasonicSensor.usleep(100_000)
+            print("Distance is", self.measured_distance, "cm")
 
     def measure(self):
         self.trigger_signal()
@@ -25,7 +37,6 @@ class UltrasonicSensor(object):
         GPIO.output(UltrasonicSensor.GPIO_PIN, GPIO.LOW)
 
     def receive_signal(self):
-        # Todo: maybe we loose time here by calling the function
         t_0 = time.time()
         GPIO.setup(UltrasonicSensor.GPIO_PIN, GPIO.IN)
         count = 0
@@ -40,7 +51,6 @@ class UltrasonicSensor(object):
         while count < UltrasonicSensor.TIMEOUT_2:
             if not GPIO.input(UltrasonicSensor.GPIO_PIN):
                 break
-            print("true")
             count += 1
         if count >= UltrasonicSensor.TIMEOUT_2:
             print("M2 failed")
@@ -55,6 +65,16 @@ class UltrasonicSensor(object):
         if measurement:
             return measurement * 1000000 / 29 / 2
 
+    def stop_measuring(self):
+        self.stop = True
+
+    def retrieve_state(self):
+        return self.measured_distance
+
+
 if __name__ == "__main__":
-    distance = UltrasonicSensor().measure()
-    print("Distance is", distance, "cm")
+    sensor = UltrasonicSensor()
+    time.sleep(2)
+    sensor.stop_measuring()
+
+
