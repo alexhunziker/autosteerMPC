0a549bb14203e18862702ea683857cef9fcfeb5c
alexhunziker91@gmail.com <alexhunziker@sunrise.ch>
2019-08-17 12:40:34 +0200

    target yaw rate added to sensor_fuser

--- coordinator/parameters.py
+++ coordinator/parameters.py
@@ -2,7 +2,15 @@ class Parameters(object):
     def __init__(self):
         self.ultrasonic = 500
         self.ultrasonic_timestamp = None
+
         self.gps = {"lat": None, "lon": None, "altitude": None, "speed": None}
         self.gps_timestamp = None
+
+        self.yaw_rate = None
+        self.yaw_rate_timestamp = None
+
         self.lidar = None
-        self.curvature_yaw = None
+        self.lidar_timestamp = None
+
+        self.speed = 0
+        self.speed_timestamp = None
--- coordinator/sensor_fuser.py
+++ coordinator/sensor_fuser.py
@@ -1,5 +1,6 @@
 from parameters import Parameters
 
+from cv import lane_recognizer
 from gps_sensor import gps_sensor
 from ultrasonic import ultrasonic_sensor
 
@@ -8,6 +9,7 @@ class SensorFuser(object):
     def __init__(self):
         self.ultrasonic = ultrasonic_sensor.UltrasonicSensor()
         self.gps = gps_sensor.GPSSensor()
+        self.lane_recognizer = lane_recognizer.LaneRecognizer()
 
     def retrieve_updates(self):
         parameters = Parameters()
@@ -17,5 +19,8 @@ class SensorFuser(object):
 
         parameters.gps = self.gps.retrieve_state()
         parameters.gps_timestamp = self.gps.last_valid
+
+        parameters.yaw_rate = self.lane_recognizer.retrieve_state()
+        parameters.yaw_rate_timestamp = self.lane_recognizer.last_valid
 
         return parameters
