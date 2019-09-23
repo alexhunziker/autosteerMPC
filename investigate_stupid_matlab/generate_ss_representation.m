896251caef5e7a0ce0d0c8c963fb3396b8ede36c
alexhunziker91@gmail.com <alexhunziker@sunrise.ch>
2019-09-23 02:07:02 +0200

    adaptable speed for cv simple

--- investigate_stupid_matlab/generate_ss_representation.m
+++ investigate_stupid_matlab/generate_ss_representation.m
@@ -11,19 +11,21 @@ Ts = 0.01;
 % Set arbitrary speed for generating the object
 Vx = 2.0;
 
-% Continuous-time model
-Ac = [0, 0, Vx, 0, -Vx;
-       0, 0, 1, 0, 0;
-       0, 0, -(cr*lr^2+cf*lf^2)/(iz*Vx), -(cr*lr-cf*lf)/iz, 0;
-       0, 0, 1-(cr*lr-cf*lf)/(m*Vx^2), -(cf+cr)/(m*Vx), 0;
-       0, 0, 0, 0, -.99];
+Ac = [1, 0, -Vx, 0, +Vx, 0;
+       0, 0, 1, 0, 0, 0;
+       0, 0, -(cr*lr^2+cf*lf^2)/(iz*Vx), -(cr*lr-cf*lf)/iz, 0, 0;
+       0, 0, 1-(cr*lr-cf*lf)/(m*Vx^2), -(cf+cr)/(m*Vx), 0, 0;
+       0, 0, 0, 0, -.99, 0;
+       0, 0, 0, 0, 0, -1/10];
        
-Bc = [0, 0, cf*lf/iz, -cf/(m*Vx), 0;
-      0, 0, 0, 0, -.99]';
+Bc = [0, 0, cf*lf/iz, -cf/(m*Vx), 0, 0;
+      0, 0, 0, 0, -.99, 0;
+      0, 0, 0, 0, 0, 1;
+      0, 0, 0, 0, 0, -2]';
  
 % Position, Yaw, Speed
-Cc = eye(5);
+Cc = eye(6);
     
-Dc = zeros(5, 2);
+Dc = zeros(6, 4);
 
 ss_model = c2d(ss(Ac, Bc, Cc, Dc), Ts);
