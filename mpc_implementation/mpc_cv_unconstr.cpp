a0bffc32b6a881a05b6af72fa8e3e2d3b52beaf0
alexhunziker91@gmail.com <alexhunziker@sunrise.ch>
2019-10-06 18:42:11 +0200

    fixed new mpc cv unconstrained

--- mpc_implementation/mpc_cv_unconstr.cpp
+++ mpc_implementation/mpc_cv_unconstr.cpp
@@ -1,5 +1,6 @@
 #include <dlib/control.h>
 #include <math.h>
+#include <dlib/string.h>
 
 #include "mpc_cv_unconstr_ss.cpp"
 
@@ -8,30 +9,33 @@ using namespace dlib;
 
 #define PI 3.14159265
 
+const bool DEBUG = true;
+
 // Constants for Model
-const int CONTROL_HORIZON = 60;
+const int CONTROL_HORIZON = 40;
 const float STEP_SIZE = 0.1;
 // Contrary to MATLAB, dlib does not allow different control/prediction horizons
 const int PREDICTION_HORIZON = CONTROL_HORIZON;
-const int NR_OF_STATES = 6;
-const int NR_OF_CONTROLS = 4;
 
-const float CRUISING_SPEED = 4.0;
+const float CRUISING_SPEED = -4.0;
 
 mpc<NR_OF_STATES, NR_OF_CONTROLS, PREDICTION_HORIZON> mpc_objects[3];
 
-mpc<NR_OF_STATES, NR_OF_CONTROLS, PREDICTION_HORIZON> initialize_for_speed(float v);
+mpc<NR_OF_STATES, NR_OF_CONTROLS, PREDICTION_HORIZON> initialize_mpc_object(int i);
+mpc<NR_OF_STATES, NR_OF_CONTROLS, PREDICTION_HORIZON> get_best_mpc_object(int speed);
 
 extern "C"
 {
     void initialize_mpc_objects()
     {
-        for (int i = 0; i <= 3; i++)
+        init_ss();
+        for (int i = 0; i < 3; i++)
         {
             mpc<NR_OF_STATES, NR_OF_CONTROLS, PREDICTION_HORIZON> mpc_object = initialize_mpc_object(i);
             mpc_objects[i] = mpc_object;
         }
-        printf("DEBUG: MPC Objects set up for CV Unconstrained.", v);
+        if (DEBUG)
+            printf("DEBUG: MPC Objects set up for CV Unconstrained.\n");
     }
 
     void predict(
@@ -48,20 +52,22 @@ extern "C"
 
         matrix<double, NR_OF_STATES, 1> target_state;
         target_state = 0, 0, yaw_rate_target, 0, yaw_rate_target, speed_target;
+        if (DEBUG)
+            cout << "DEBUG: target state is: " << trans(target_state) << "\n";
         mpc_controller.set_target(target_state);
 
         matrix<double, NR_OF_CONTROLS, 1> action = mpc_controller(current_state);
-        cout << "INFO: best controll: " << trans(action);
+        cout << "INFO: best controll: " << trans(action) << "\n";
 
         for (int i = 0; i < NR_OF_CONTROLS; i++)
         {
             controls[i] = action(i, 0);
         }
 
-        matrix<double, NR_OF_STATES, 1> predicted_state = mpc_controller.getA() * current_state + mpc_controller.getB() * action;
+        matrix<double, NR_OF_STATES, 1> calc_state = mpc_controller.get_A() * current_state + mpc_controller.get_B() * action;
         for (int i = 0; i < NR_OF_STATES; i++)
         {
-            predicted_state[i] = predicted_state(i, 0);
+            predicted_state[i] = calc_state(i, 0);
         }
     }
 }
@@ -84,7 +90,7 @@ mpc<NR_OF_STATES, NR_OF_CONTROLS, PREDICTION_HORIZON> initialize_mpc_object(int 
 
     // Matrix Q: Costs of divergence in states
     matrix<double, NR_OF_STATES, 1> Q;
-    Q = 5, 0, 1, 0, 10, 1;
+    Q = 5, 0, 2, 0, 10, 1;
 
     // Matrix R: Costs of controls
     matrix<double, NR_OF_CONTROLS, 1> R;
@@ -93,8 +99,8 @@ mpc<NR_OF_STATES, NR_OF_CONTROLS, PREDICTION_HORIZON> initialize_mpc_object(int 
     // upper, lower: Control limits
     matrix<double, NR_OF_CONTROLS, 1> upper;
     matrix<double, NR_OF_CONTROLS, 1> lower;
-    upper = 0.78, 999, 1, 1;
-    lower = -0.78, -999, 0, 0;
+    upper = 0.78, 1, 1, 1;
+    lower = -0.78, -1, 0, 0;
 
     mpc<NR_OF_STATES, NR_OF_CONTROLS, PREDICTION_HORIZON> mpc_controller(A, B, C, Q, R, lower, upper);
 
--- mpc_implementation/mpc_cv_unconstr_test.cpp
+++ mpc_implementation/mpc_cv_unconstr_test.cpp
@@ -9,11 +9,11 @@ using namespace dlib;
 
 #define PI 3.14159265
 
-static double TARGET_YAW = 0.5;
+static double TARGET_YAW = -0.5;
 static double TARGET_X = 10;
 static double TARGET_Y = 10;
 static double TARGET_SPEED = 4;
-static double TIME_STEP = 0.01;
+static double TIME_STEP = 0.1;
 
 double accumulated_diff = 0.0;
 double x_calc = 0.0;
@@ -21,6 +21,49 @@ double y_calc = 0.0;
 double yaw_calc = 0.0;
 double sanitized_yaw = 0.0;
 double offset = 4;
+
+double get_y_offset(double v, double target_yaw_rate, double actual_yaw_rate)
+{
+    // This is an approximation, assumes constant speed. But is used for testing only
+    double correction_angle = (actual_yaw_rate - target_yaw_rate) + accumulated_diff;
+    cout << "DEBUG: this diff is " << actual_yaw_rate << "-" << target_yaw_rate << "=" << (actual_yaw_rate - target_yaw_rate) << "\n";
+    accumulated_diff += (actual_yaw_rate - target_yaw_rate) * TIME_STEP; // is this true?
+    offset += correction_angle * v * TIME_STEP * -1;
+    cout << "Calculated Y-Offset is: " << offset << "\n";
+    return offset;
+}
+
+double sign(double x)
+{
+    if (x > 0.0)
+        return 1;
+    else
+        return -1;
+}
+
+void calc_position(double yaw, double yaw_rate, double v)
+{
+    x_calc += cos(yaw) * v * TIME_STEP;
+    y_calc += sin(yaw) * v * TIME_STEP;
+    yaw_calc += yaw_rate * TIME_STEP;
+
+    double yaw_off = yaw_calc - PI;
+    sanitized_yaw = fmod(abs(yaw_off), (2 * PI)) * sign(yaw_off) + PI;
+    cout << "Predicted position is: x= " << x_calc << ", y= " << y_calc << ", yaw= " << sanitized_yaw << "\n";
+}
+
+double get_target_speed()
+{
+    double diff_x = x_calc - TARGET_X;
+    double diff_y = y_calc - TARGET_Y;
+    double distance_speed = sqrt(pow(diff_x, 2) + pow(diff_y, 2)) / 2;
+
+    double yaw_speed = (TARGET_SPEED - 3 * TARGET_YAW) + 1.5;
+    double current_appropriate_speed = min(yaw_speed, distance_speed);
+    double target_speed = min(current_appropriate_speed, TARGET_SPEED);
+    cout << "Target speed is: " << target_speed << "\n";
+    return target_speed;
+}
 
 int main(int argc, char **argv)
 {
@@ -32,43 +75,12 @@ int main(int argc, char **argv)
     for (int i = 1; i < 1000; i++)
     {
         calc_position(state[1], state[2], state[5]);
-        predict(get_speed_target(), x_calc, y_calc, state[5], get_y_offset(), sanitized_yaw, state[2], state[3], TARGET_YAW, controls, state);
-        cout << "Predicted state is: " << state << "\n";
+        predict(get_target_speed(), x_calc, y_calc, state[5], get_y_offset(state[5], TARGET_YAW, state[2]), state[1], state[2], state[3], TARGET_YAW, controls, state);
+        cout << "Predicted state is: ";
+        for (int i = 0; i < NR_OF_STATES; i++)
+            cout << state[i] << ", ";
+        cout << "\n";
     }
 
     return 0;
-}
-
-double get_y_offset(double v, double target_yaw_rate, double actual_yaw_rate)
-{
-    // This is an approximation, assumes constant speed. But is used for testing only
-    double correction_angle = (actual_yaw_rate - target_yaw_rate) + accumulated_diff;
-    accumulated_diff += (actual_yaw_rate - target_yaw_rate) * TIME_STEP; // is this true?
-    offset += correction_angle * v * TIME_STEP;
-    cout << "Calculated Y-Offset is: " << offset;
-    return offset
-}
-
-void calc_position(double yaw, double yaw_rate, double v)
-{
-    x_calc += cos(yaw) * v * TIME_STEP;
-    y_calc += sin(yaw) * v * TIME_STEP;
-    yaw_calc += yaw_rate * TIME_STEP;
-
-    double yaw_off = yaw_calc - PI;
-    sanitized_yaw = mod(abs(yaw_off), 2 * PI) * sign(yaw_off) + PI;
-    cout << "Predicted position is: x= " << x_calc << ", y= " << y_calc << ", yaw= " << sanitized_yaw << "\n";
-}
-
-double get_target_speed()
-{
-    double diff_x = x_calc - TARGET_X;
-    double diff_y = y_calc - TARGET_Y;
-    double distance_speed = sqrt(pow(diff_x, 2) + pow(diff_y, 2)) / 2;
-
-    double yaw_speed = TARGET_SPEED - 3*TARGET_YAW) + 1.5;
-    double current_appropriate_speed = min(yaw_speed, distance_speed);
-    double target_speed = min(current_appropriate_speed, yaw_speed);
-    cout << "Target speed is: " << target_speed << "\n";
-    return target_speed;
 }
