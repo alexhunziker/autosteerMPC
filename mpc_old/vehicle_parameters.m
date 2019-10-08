1f0e1e4698f1b5bff4b0f039b63dfd1ddbe3a2ff
alexhunziker91@gmail.com <alexhunziker@sunrise.ch>
2019-10-09 00:37:46 +0200

    Removed old MATLAB simulation

--- mpc_old/vehicle_parameters.m
+++
@@ -1,6 +0,0 @@
-m = 30;     % Mass
-lf = 0.7;   % Distance mass center/front wheel
-lr = 0.3;   % Distance mass center/rear wheel
-cf = 3000;   % Cornering Stiffness front
-cr = 3000;   % Cornering Stiffness rear
-iz = 100;   % Yaw Moment of Innertia?
