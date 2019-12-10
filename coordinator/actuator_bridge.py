cfd1d9a2392d9413aeec6a76547eaac116c2ef51
Alex Hunziker <alexhunziker@sunrise.ch>
2019-12-10 01:01:51 +0100

    twerking actuator bridge, introduce smoothing to steering corrected conversions

--- coordinator/actuator_bridge.py
+++ coordinator/actuator_bridge.py
@@ -8,7 +8,7 @@ from impulses import Impulses
 
 class ActuatorBridge(object):
 
-    def __init__(self, mock=False):
+    def __init__(self, mock=False, smoothing=True):
         if mock:
             self.send = self.mock_send
             return
@@ -19,6 +19,7 @@ class ActuatorBridge(object):
         except:
             print("ERROR: Connection with Arduino failed.")
         self.busy = False # Because the vehicle is slow with processing, otherwise signals get lost
+        self.smoothing = smoothing
 
     def send(self, impulses):
         if self.busy:
@@ -29,24 +30,31 @@ class ActuatorBridge(object):
             print("WARN: Arduino not available. No commands were sent")
             return
         print("INFO: Throttle", impulses.throttle, "Breaks", impulses.breaks, "Steering", impulses.steering)
-        # Let's try to send an impuls all the time... Since some writes are ignored by the Arduino... Viel hilft vieleicht viel
-        if impulses.throttle != self.last_impulses.throttle or impulses.breaks != self.last_impulses.breaks or impulses.steering != self.last_impulses.steering or True: 
-            # Steering angle in degrees, plus offset
-            if abs(impulses.steering-self.last_impulses.steering)>0.05:
-                impulses.steering = 0.05*np.sign(impulses.steering-self.last_impulses.steering)
-                print("INFO: Smoothened steering")
-            steering_angle = str(int(impulses.steering  / 3.14 * 180) + 50)
-            if impulses.throttle > 0.4:
-                impulses.throttle = 0.4
-                print("INFO: Throttle adjusted")
-            throttle = str(int(impulses.throttle*10));
-            # the breaks are fragile, therefore intensity is always kept below lvl 6
-            if impulses.breaks > 0.6:
-                impulses.breaks = 0.6
-            break_intensity = str(int(impulses.breaks*10))
-            control_string = "<" + steering_angle + throttle + break_intensity + ">"
-            self.arduino.write(control_string.encode('utf-8'))
-            time.sleep(0.1)                                 # Give Arduino a bit time
+
+        # Steering angle in degrees, inverted, plus offset
+        if self.smoothing and abs(impulses.steering-self.last_impulses.steering)>0.05:
+            print(np.sign(impulses.steering-self.last_impulses.steering))
+            tmp_max_allowed = 0.05*np.sign(impulses.steering-self.last_impulses.steering)+self.last_impulses.steering
+            if abs(tmp_max_allowed)<abs(impulses.steering):
+                impulses.steering = tmp_max_allowed
+                print("INFO: Smoothened steering to", impulses.steering)
+        steering_angle = str(int(-impulses.steering / 3.14 * 180) + 50)
+        # The engine is powerful, limit acceleration and speed
+
+        if impulses.throttle > 0.5:
+            impulses.throttle = 0.5
+            print("INFO: Throttle adjusted")
+        throttle = str(int(impulses.throttle*10))
+
+        # the breaks are fragile, therefore intensity is always kept below lvl 6
+        if impulses.breaks > 0.6:
+            impulses.breaks = 0.6
+        break_intensity = str(int(impulses.breaks*10))
+
+        control_string = "<" + steering_angle + throttle + break_intensity + ">"
+        self.arduino.write(control_string.encode('utf-8'))
+        print("DEBUG: Control string sent", control_string)
+        time.sleep(0.1)                                 # Give Arduino a bit time
         self.last_impulses = impulses
         self.busy = False
 
@@ -56,25 +64,32 @@ class ActuatorBridge(object):
 
 if __name__ == "__main__":
     try:
-        actuatorBridge = ActuatorBridge()
-        #for i in range(-30, 30, 5):
-        #    print("Test steering; ", i/100)
-        #    actuatorBridge.send(Impulses(i/100, 0, 0))
-        #    time.sleep(0.2)
-        #for i in range(0, 5, 1):
-        #    print("Test throttle; ", i/10)
-        #    actuatorBridge.send(Impulses(0, i/10, 0))
-        #    time.sleep(2)
-        actuatorBridge.send(Impulses(0.0, 0.0, 0.0))
+        actuatorBridge = ActuatorBridge(smoothing=False)
+        time.sleep(3)
+
+        print("Test Steering...")
+        actuatorBridge.send(Impulses(-0.3, 0.0, 0.0)) # left
         time.sleep(2)
-        #actuatorBridge.send(Impulses(0, 0.3, 0.0))
-        #time.sleep(2)
+        actuatorBridge.send(Impulses(0.3, 0.0, 0.0)) # right
+        time.sleep(2)
+        print("Test Acceleration...")
+        actuatorBridge.send(Impulses(0, 1, 0.0))
+        time.sleep(0.5)
+        print("Test Brakes...")
+        actuatorBridge.send(Impulses(0, 0, 1))
+        time.sleep(0.5)
+        print("done.")
+        actuatorBridge.send(Impulses(0, 0, 0))
+
+        print("Test loop...")
         while True:
-            actuatorBridge.send(Impulses(0.1, 0.3, 0.0))
-            actuatorBridge.send(Impulses(0.0, 0.0, 0.0))
-            print("set")
-        actuatorBridge.send(Impulses(0.0, 0.0, 0.0))
-        print("DONE")
+            actuatorBridge.send(Impulses(-0.2, 1.0, 0.0))
+            time.sleep(2)
+            actuatorBridge.send(Impulses(0.2, 0.0, 0.0))
+            time.sleep(2)
+        time.sleep(2)
+        actuatorBridge.send(Impulses(0, 0.0, 0.0))
+        print("done.")
     except KeyboardInterrupt:
         print("INFO: Shutting down")
         ActuatorBridge(mock=False).send(Impulses(0, 0, 0))
